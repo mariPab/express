e20928a683194672755e481e7851ab9f4eed7488
Marysia Pabich <marysia.pabich@gmail.com>
2020-02-16 13:52:29 +0100

    Add page About

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ app.get('/home', (req, res) => {
     res.show('index.html');
 });
 
+app.get('/about', (req, res) => {
+    res.show('about.html');
+});
+
 app.listen(8000, () => {
     console.log('Server is running on port 8000');
 });
