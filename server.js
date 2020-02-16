18e07f7837a9e7bd0649303a479a7bd9733adaaa
Marysia Pabich <marysia.pabich@gmail.com>
2020-02-16 08:38:14 +0100

    Add middleware function to get files & main page

--- server.js
+++ server.js
@@ -3,6 +3,21 @@ const path = require('path');
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.show = name => {
+        res.sendFile(path.join(__dirname + `/views/${name}`))
+    };
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.show('index.html');
+});
+
+app.get('/home', (req, res) => {
+    res.show('index.html');
+});
+
 app.listen(8000, () => {
     console.log('Server is running on port 8000');
 });
